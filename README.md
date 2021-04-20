9a93e7bf09e4cf4d19c734a95c288fc598931e32
Matthew Kindzerske <kindzerske@gmail.com>
2021-04-20 16:49:04 -0400

    renders simple html

---
+++ README.md
@@ -0,0 +1,11 @@
+# CodeChallenge2 - Pomodoro Timer
+
+An app for the appDev challenge.
+
+(no deployments yet, and in progress)
+
+## Tagged
+
+(insert link)
+
+And creating the template for Serverless, AWS, NodeJS: `serverless create --template aws-nodejs`
--- handler.js
+++ handler.js
@@ -3,16 +3,7 @@
 module.exports.hello = async (event) => {
   return {
     statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
+    headers: { 'Content-Type': 'text/html'},
+    body: "<html><body><h1>Serverless, NodeJS, HTML</h1></body></html>"
   };
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
