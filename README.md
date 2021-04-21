02ec86d8712a570735d72c2ec020147717b79b57
Matthew Kindzerske <kindzerske@gmail.com>
2021-04-21 09:09:55 -0400

    html has js, setting basic cookie with expiry

--- README.md
+++ README.md
@@ -6,6 +6,6 @@ An app for the appDev challenge.
 
 ## Tagged
 
-(insert link)
+Serverless, NodeJS, HTML bare minimum, [tagged](https://github.com/yamori/challenge2_pomodoroTimer/releases/tag/v0.1_serverless_nodeJS_html).
 
 And creating the template for Serverless, AWS, NodeJS: `serverless create --template aws-nodejs`
--- handler.js
+++ handler.js
@@ -4,6 +4,16 @@ module.exports.hello = async (event) => {
   return {
     statusCode: 200,
     headers: { 'Content-Type': 'text/html'},
-    body: "<html><body><h1>Serverless, NodeJS, HTML</h1></body></html>"
+    body: 
+`
+<html>
+  <head>
+  <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+  </head>
+  <body>
+    <h1>Serverless, NodeJS, HTML</h1>
+  </body>
+</html>
+`
   };
 };
